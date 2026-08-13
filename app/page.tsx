5ebe3b2ec9091f65218118a0e416e5043a10c954
James Campbell <jscampbell43@gmail.com>
2026-08-13 16:06:04 -0700

    Wrapped Search components in Suspense Boundary with loading fallbacks

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,42 @@
 import Link from "next/link";
 import Search from '@/app/ui/search';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
         
-        <div className="flex gap-20 w-full mb-8">
-          {/* Search Actor 1 */}
-          <div className="flex-1">
-            <label htmlFor="actor1" className="mb-2 block text-sm font-medium">
-              Choose Actor 1
-            </label>
-            <div className="relative">
-              <Search
-                placeholder = "Search an actor by name"
-                queryKey = "actor1"
-              />
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="flex gap-20 w-full mb-8">
+            {/* Search Actor 1 */}
+            <div className="flex-1">
+              <label htmlFor="actor1" className="mb-2 block text-sm font-medium">
+                Choose Actor 1
+              </label>
+              <div className="relative">
+                <Search
+                  placeholder = "Search an actor by name"
+                  queryKey = "actor1"
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Search Actor 2 */}
-          <div className="flex-1">
-            <label htmlFor="actor2" className="mb-2 block text-sm font-medium">
-              Choose Actor 2
-            </label>
-            <div className="relative">
-              <Search
-                placeholder = "Search an actor by name"
-                queryKey = "actor2"
-              />
+            {/* Search Actor 2 */}
+            <div className="flex-1">
+              <label htmlFor="actor2" className="mb-2 block text-sm font-medium">
+                Choose Actor 2
+              </label>
+              <div className="relative">
+                <Search
+                  placeholder = "Search an actor by name"
+                  queryKey = "actor2"
+                />
+              </div>
             </div>
+          
           </div>
-        
-        </div>
+        </Suspense>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <Link
             className="flex h-12 w-full items-center justify-center gap-20 rounded-full px-5 bg-[#3B1299] transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
